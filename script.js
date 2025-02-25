d5cdf59326987f13567533cd2a1e43ffb3118682
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-25 12:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+document.addEventListener("DOMContentLoaded", function() {
+  const list = document.getElementById("infi-list");
+
+  function addItems(num) {
+    for (let i = 0; i < num; i++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Item ${list.children.length + 1}`;
+      list.appendChild(listItem);
+    }
+  }
+
+ 
+  addItems(10);
+
+  list.addEventListener("scroll", function() {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+      
+      addItems(2);
+    }
+  });
+});
